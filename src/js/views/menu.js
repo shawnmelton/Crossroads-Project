9569a165641d5ea051f1ef3f1fed179f7ac4ed74
Shawn Melton <shawn.melton@techs-MacBook-Pro-35.local>
2016-10-22 18:27:38 -0400

    Correcting menu for mobile

--- src/js/views/menu.js
+++ src/js/views/menu.js
@@ -1,9 +1,30 @@
 define(['jquery', 'backbone', 'templates/html.jst'], 
     function($, Backbone, htmlJST) {
         var initialized = false,
+            showMenu = false,
+            menu = document.getElementById('menu'),
+            menuBtn = document.getElementById('menu-btn'),
+            menuOverlay = document.getElementById('menu-overlay'),
+
+            clickCallback = function(e) {
+                e.preventDefault();
+
+                showMenu = !showMenu;
+
+                if (showMenu) {
+                    menu.classList.add('active');
+                    menuBtn.classList.add('active');
+                    menuOverlay.classList.add('active');
+                } else {
+                    menu.classList.remove('active');
+                    menuBtn.classList.remove('active');
+                    menuOverlay.classList.remove('active');
+                }
+            },
+
             menuCallback = function(r) {
                 if (typeof r === 'object' && r.response !== null) {
-                    document.getElementById('menu').innerHTML = JST['src/js/templates/menu.html']({
+                    menu.innerHTML = JST['src/js/templates/menu.html']({
                         links: r.response
                     });
                 }
@@ -17,7 +38,14 @@ define(['jquery', 'backbone', 'templates/html.jst'],
                         $.getJSON('/api', {
                             menu: 1
                         }, menuCallback);
+
+                        this.setEvent();
                     }
+                },
+
+                setEvent: function() {
+                    menuBtn.addEventListener('click', clickCallback, false);
+                    menuOverlay.addEventListener('click', clickCallback, false);
                 }
             });
         
